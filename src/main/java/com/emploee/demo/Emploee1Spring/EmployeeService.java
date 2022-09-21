bd04027db244fe7f59ed5179732576cff2788083
KiriukhinD <dm.arz2018@yandex.ru>
2022-09-21 11:40:16 +0300

    one 1

---
+++ src/main/java/com/emploee/demo/Emploee1Spring/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.emploee.demo.Emploee1Spring;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+    private static Employee[] employees = new Employee[10];
+
+
+}
